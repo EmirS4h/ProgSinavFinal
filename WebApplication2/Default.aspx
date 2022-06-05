7da23a1c3d052f698759e601c6b944a0c2e65d87
Emir <96378991+EmirS4h@users.noreply.github.com>
2022-06-05 23:06:17 +0300

    Add project files.

---
+++ WebApplication2/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication2.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="Numara" runat="server"></asp:TextBox>
+            <asp:Button ID="EkleBtn" runat="server" Text="EKLE File" OnClick="EkleBtn_Click" />
+            <asp:Button ID="OkuBtn" runat="server" Text="OKU File" OnClick="OkuBtn_Click"/>
+            <asp:Button ID="DegistirBtn" runat="server" Text="Değiştir File" OnClick="DegistirBtn_Click"/>
+        </div>
+        <div>
+            <asp:Button ID="ListeleBtn" runat="server" Text="Listele SQL" OnClick="ListeleBtn_Click" />
+            <asp:Button ID="EkleBtnSql" runat="server" Text="Ekle SQL" OnClick="EkleBtnSql_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication2/Default.aspx.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void EkleBtn_Click(object sender, EventArgs e)
+        {
+            // StreanWriter dosya yoksa Dosyayı oluşturur
+            // Server.MapPath dosyayı bulur
+            StreamWriter dosya = new StreamWriter(Server.MapPath("ogrenci.txt"), true); // Eger true varsa dosyanın üzerine yazar, önceki yazılanları silmez
+            string line = Numara.Text; // yazılacak satır
+            dosya.WriteLine(line); // dosyaya yaz
+            dosya.Close(); // dosyayı kapat
+        }
+
+        protected void OkuBtn_Click(object sender, EventArgs e)
+        {
+            // StreamReader Dosya okur
+            StreamReader dosya = new StreamReader(Server.MapPath("ogrenci.txt"));
+            string satir; // Okunan satırları tutar
+
+            // okunacak satır kalmayana kadar devam eder ve her satırı 'satir' degiskenine ekler
+            while ((satir = dosya.ReadLine()) != null)
+            {
+                Response.Write(satir + "</br>"); // Her satır okunusunda Ekrana yazdırır
+            }
+            dosya.Close(); // Dosyayı kapatır
+        }
+
+        protected void DegistirBtn_Click(object sender, EventArgs e)
+        {
+            // Okunacak dosya
+            StreamReader dosya1 = new StreamReader(Server.MapPath("ogrenci.txt"));
+
+            // Üsteki dosyanın yerini alacak dosya
+            StreamWriter dosya2 = new StreamWriter(Server.MapPath("b.txt"));
+
+            string satir;
+
+            // yine okunacak satır kalmayana kadar devam
+            while ((satir = dosya1.ReadLine()) != null)
+            {
+                if (satir != Numara.Text) // eger satır silinecek değil ise o satırı dosya2 ye yaz
+                {
+                    dosya2.WriteLine(satir);
+                }
+                else // eger satır silinmesini istediğimiz satır ise bir şey yapma
+                {
+                    continue; // bir şey yapmadığımız için o satırı yeni dosyaya yazmayacak ve silinmiş gibi olucak
+                }
+            }
+
+            // Dosyaları kapat
+            dosya1.Close();
+            dosya2.Close();
+
+            // Eski dosyayı sil
+            File.Delete(Server.MapPath("ogrenci.txt"));
+
+            // Yeni dosyaya eskisinin adını ver
+            File.Move(Server.MapPath("b.txt"), Server.MapPath("ogrenci.txt"));
+
+        }
+
+        protected void ListeleBtn_Click(object sender, EventArgs e)
+        {
+            String path = Server.MapPath("ogrenciler.db");
+            String connection = "Data Source="+path+";Version=3;";
+            String command = "SELECT * FROM tbl_ogrenciler;";
+
+            SQLiteConnection conn = new SQLiteConnection(connection);
+
+            try
+            {
+                conn.Open();
+                SQLiteCommand cmd = new SQLiteCommand(command, conn);
+
+                // geriye bir şey dönüyorsa
+                // bu durumda numaralar dönüyor
+                SQLiteDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Response.Write(
+                        reader["numara"].ToString() + "</br>");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        protected void EkleBtnSql_Click(object sender, EventArgs e)
+        {
+            String path = Server.MapPath("ogrenciler.db");
+            String connection = "Data Source="+path+";Version=3;";
+            String command = "INSERT INTO tbl_ogrenciler (numara) VALUES("+Numara.Text+");";
+
+            SQLiteConnection conn = new SQLiteConnection(connection);
+
+            try
+            {
+                conn.Open();
+                SQLiteCommand cmd = new SQLiteCommand(command, conn);
+
+                //SQLiteDataReader reader = cmd.ExecuteReader();
+
+                cmd.ExecuteNonQuery();
+
+                //while (reader.Read())
+                //{
+                //    Response.Write(
+                //        reader["numara"].ToString() + "</br>");
+                //}
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
